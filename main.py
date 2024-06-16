bcf69e3be9ac462e264426f5f1fc0ddde12561bd
Ansa2610 <asavilova@gmail.com>
2024-06-16 15:57:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+a = "длина слова"
+print(len(a))
+
+first = 100
+second = 101
+summa = first + second
+print(summa)
+diff = first - second
+print(diff)
+
+first = 1
+second = 2
+third = 3
+mean = (first + second + third) / 3
+print(mean)
+
+first_string = "Вторник"
+second_string = ("Понедельник")
+print(second_string + "," + first_string)
+
+a = 1
+b = 2
+c = 3
+f = (a*b) + (a*c)
+print(f)
+f = 5
+print(f ** 3 / 2)
